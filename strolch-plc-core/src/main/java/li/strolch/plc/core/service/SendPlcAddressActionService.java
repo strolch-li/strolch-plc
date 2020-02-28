6205ccc8b984c4cbd61352c2d6b1c43da017e38a
Robert von Burg <eitch@eitchnet.ch>
2020-02-28 09:23:35 +0100

    [Minor] No TX needed in SendPlcAddressActionService

--- strolch-plc-core/src/main/java/li/strolch/plc/core/service/SendPlcAddressActionService.java
+++ strolch-plc-core/src/main/java/li/strolch/plc/core/service/SendPlcAddressActionService.java
@@ -2,11 +2,10 @@ package li.strolch.plc.core.service;
 
 import static li.strolch.plc.model.PlcConstants.*;
 
+import com.google.gson.JsonObject;
 import li.strolch.plc.core.PlcHandler;
 import li.strolch.plc.model.PlcAddress;
 import li.strolch.plc.model.PlcAddressType;
-import com.google.gson.JsonObject;
-import li.strolch.persistence.api.StrolchTransaction;
 import li.strolch.service.JsonServiceArgument;
 import li.strolch.service.api.AbstractService;
 import li.strolch.service.api.ServiceResult;
@@ -31,25 +30,19 @@ public class SendPlcAddressActionService extends AbstractService<JsonServiceArgu
 		String action = jsonObject.get(PARAM_ACTION).getAsString();
 		String valueS = jsonObject.get(PARAM_VALUE).getAsString();
 
-		try (StrolchTransaction tx = openArgOrUserTx(arg)) {
+		PlcHandler plcHandler = getComponent(PlcHandler.class);
+		PlcAddress plcAddress = plcHandler.getPlcAddress(resource, action);
 
-			PlcHandler plcHandler = getComponent(PlcHandler.class);
-			PlcAddress plcAddress = plcHandler.getPlcAddress(resource, action);
+		Object value = plcAddress.valueType.parseValue(valueS);
 
-			Object value = plcAddress.valueType.parseValue(valueS);
-
-			if (addressType == PlcAddressType.Telegram) {
-				logger.info("PLC Send " + resource + "-" + action + " with " + valueS);
-				plcHandler.send(resource, action, value);
-			} else if (addressType == PlcAddressType.Notification) {
-				logger.info("PLC Notification " + resource + "-" + action + " with " + valueS);
-				plcHandler.notify(resource, action, value);
-			} else {
-				throw new UnsupportedOperationException("Unhandled address type " + addressType);
-			}
-
-			if (tx.needsCommit())
-				tx.commitOnClose();
+		if (addressType == PlcAddressType.Telegram) {
+			logger.info("PLC Send " + resource + "-" + action + " with " + valueS);
+			plcHandler.send(resource, action, value);
+		} else if (addressType == PlcAddressType.Notification) {
+			logger.info("PLC Notification " + resource + "-" + action + " with " + valueS);
+			plcHandler.notify(resource, action, value);
+		} else {
+			throw new UnsupportedOperationException("Unhandled address type " + addressType);
 		}
 
 		return ServiceResult.success();
